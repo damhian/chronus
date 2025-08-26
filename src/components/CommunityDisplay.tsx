47c4742eee8b850b76b97ddf39e3239d35daac8c
damhian <david.mayshiogie@protonmail.com>
2025-08-26 08:24:00 +0700

    add the capability to get communities data by Id

--- src/components/CommunityDisplay.tsx
+++ src/components/CommunityDisplay.tsx
@@ -13,6 +13,13 @@ interface PostWithCommunity extends Post {
   };
 }
 
+interface community {
+  id: number;
+  name: string;
+  description: string;
+  created_at: string;
+}
+
 export const fetchCommunityPost = async (
   communityId: number,
 ): Promise<PostWithCommunity[]> => {
@@ -23,37 +30,71 @@ export const fetchCommunityPost = async (
     .order("created_at", { ascending: false });
 
   if (error) throw new Error(error.message);
+
   return data as PostWithCommunity[];
 };
 
+const fetchCommunityById = async (communityId: number): Promise<community> => {
+  const { data, error } = await supabase
+    .from("communities")
+    .select("*")
+    .eq("id", communityId)
+    .single();
+
+  if (error) throw new Error(error.message);
+  return data as community;
+};
+
 export const CommunityDisplay = ({ communityId }: Props) => {
-  const { data, error, isLoading } = useQuery<PostWithCommunity[], Error>({
+  const {
+    data: postsData,
+    error: postsError,
+    isLoading: postsIsLoading,
+  } = useQuery<PostWithCommunity[], Error>({
     queryKey: ["communityPost", communityId],
     queryFn: () => fetchCommunityPost(communityId),
   });
 
-  if (isLoading)
+  const {
+    data: communityData,
+    error: communityError,
+    isLoading: communityIsLoading,
+  } = useQuery<community, Error>({
+    queryKey: ["community", communityId],
+    queryFn: () => fetchCommunityById(communityId),
+  });
+
+  if (postsIsLoading || communityIsLoading)
     return <div className="text-center py-4">Loading communities...</div>;
-  if (error)
+  if (postsError)
     return (
       <div className="text-center text-red-500 py-4">
-        Error: {error.message}
+        Error: {postsError.message}
       </div>
     );
+
+  if (communityError)
+    return (
+      <div className="text-center text-red-500 py-4">
+        Error: {communityError.message}
+      </div>
+    );
+
   return (
     <div>
       <h2 className="text-6xl font-bold mb-6 text-center bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent">
-        {data && data[0].communities.name} Community Posts
+        {(postsData && postsData[0]?.communities?.name) || communityData?.name}{" "}
+        Community Posts
       </h2>
 
-      {data && data.length > 0 ? (
+      {postsData && postsData.length > 0 ? (
         <div className="flex flex-wrap gap-6 justify-center">
-          {data.map((post) => (
+          {postsData.map((post) => (
             <PostItem key={post.id} post={post} />
           ))}
         </div>
       ) : (
-        <p className="text-center text-gray-400">
+        <p className="text-center text-gray-400 mt-10">
           No posts in this community yet.
         </p>
       )}
